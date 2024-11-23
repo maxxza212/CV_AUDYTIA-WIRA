1bed77271df02d3292844bd3dd9189d9b22d920d
Audytia Wira <admajaaudytia@gmail.com>
2024-11-23 09:24:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+var typingEffect = new Typed(".multiText", {
+    strings: ["Audytia", "Wira", "Admaja"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500,
+});
